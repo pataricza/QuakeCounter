a6450cdfe5ec46b8237912cedd09b4262ba6ef4e
Pataricza György <pataricza1987@gmail.com>
2018-07-04 20:20:37 +0200

    feat(QuakeCounter): mega health counter ready also

--- app/src/main/java/com/example/patariczagyorgy/quakecounter/MainActivity.java
+++ app/src/main/java/com/example/patariczagyorgy/quakecounter/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends Activity {
       @Override
       public void onClick(View v) {
         redArmorCounter.startMatchTimer();
+        megaHealthCounter.startMatchTimer();
       }
     });
   }
--- app/src/main/java/com/example/patariczagyorgy/quakecounter/counter/MegaHealthCounter.java
+++ app/src/main/java/com/example/patariczagyorgy/quakecounter/counter/MegaHealthCounter.java
@@ -1,10 +1,57 @@
 package com.example.patariczagyorgy.quakecounter.counter;
 
+import android.os.CountDownTimer;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MegaHealthCounter {
 
+    private boolean megaTimerRunning;
+    private long megaHealthMilisec = 36999;
+    private TextView megaHealthTime;
+    private CountDownTimer megaCountDownTimer;
 
     public MegaHealthCounter(TextView megaHealthTime) {
+        this.megaHealthTime = megaHealthTime;
+    }
+
+    public void startMatchTimer() {
+        if(megaTimerRunning) {
+            stopMegaTimer();
+        } else {
+            startMegaTimer();
+        }
+    }
+
+    private void startMegaTimer() {
+        megaCountDownTimer = new CountDownTimer(megaHealthMilisec, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                megaHealthMilisec = millisUntilFinished;
+                upDateMegaTime();
+            }
+
+            @Override
+            public void onFinish() {
+                this.start();
+            }
+        }.start();
+
+        megaTimerRunning = true;
+    }
+
+    public void upDateMegaTime() {
+        int minutes = (int) (megaHealthMilisec / 1000) / 60;
+        int seconds = (int) (megaHealthMilisec / 1000) % 60;
+
+        String megaTimer = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds-1);
+
+        megaHealthTime.setText(megaTimer);
+    }
+
+    private void stopMegaTimer() {
+        megaCountDownTimer.cancel();
+        megaTimerRunning = false;
     }
 }
